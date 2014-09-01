b680233a273b79523c846c47e72ec6e2ca54de90
ubuntu12-04 <kachon@ubuntu.(none)>
2014-09-01 09:13:34 -0700

    add test.txt

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,7 @@ VideoApp::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
 end
